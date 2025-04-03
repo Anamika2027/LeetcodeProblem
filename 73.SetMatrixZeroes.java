e568e7c1140b6296b387b1284d97fa91a25bf289
Anamika2027 <152776977+Anamika2027@users.noreply.github.com>
2025-04-03 20:04:48 +0530

    73.SetMatrixZeroes.java

--- 73.SetMatrixZeroes.java
+++ 73.SetMatrixZeroes.java
@@ -4,11 +4,11 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        // Arrays to mark rows and columns that need to be set to zero
+        
         boolean[] rowZero = new boolean[m];
         boolean[] colZero = new boolean[n];
 
-        // Step 1: Identify which rows and columns should be zeroed
+       
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -18,7 +18,7 @@ class Solution {
             }
         }
 
-        // Step 2: Set the marked rows to zero
+        
         for (int i = 0; i < m; i++) {
             if (rowZero[i]) {
                 for (int j = 0; j < n; j++) {
@@ -27,7 +27,7 @@ class Solution {
             }
         }
 
-        // Step 3: Set the marked columns to zero
+        
         for (int j = 0; j < n; j++) {
             if (colZero[j]) {
                 for (int i = 0; i < m; i++) {
@@ -37,11 +37,11 @@ class Solution {
         }
     }
 
-    // Main method for testing
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input matrix dimensions
+       
         System.out.print("Enter number of rows: ");
         int m = sc.nextInt();
         System.out.print("Enter number of columns: ");
@@ -49,7 +49,7 @@ class Solution {
 
         int[][] matrix = new int[m][n];
 
-        // Reading the matrix
+    
         System.out.println("Enter elements of the matrix:");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -57,11 +57,10 @@ class Solution {
             }
         }
 
-        // Creating an object of Solution class
         Solution solution = new Solution();
         solution.setZeroes(matrix);
 
-        // Output the modified matrix
+ 
         System.out.println("\nModified Matrix:");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
