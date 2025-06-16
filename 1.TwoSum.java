6b2e97b980694f1891ccd6fd193ac04393eabec9
{anamika_singh} <ianamikas2017@gmail.com>
2025-06-16 18:27:48 +0530

    Space Complexity: O(1) & Time Complexity:O(n)

--- 1.TwoSum.java
+++ 1.TwoSum.java
@@ -22,3 +22,23 @@ class Solution {
         System.out.println(Arrays.toString(result));
     }
 }
+
+//OR in C++
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> ans;
+//         int n=nums.size();
+//         int i=0,j=n-1;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     ans.push_back(i);
+//                     ans.push_back(j);
+//                     return ans;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
