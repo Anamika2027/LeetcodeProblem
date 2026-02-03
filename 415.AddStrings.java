50d7e17f34d8bf900ab842045f46278e3cc66fa6
{anamika_singh} <ianamikas2017@gmail.com>
2026-02-03 10:55:27 +0530

    Time complexity: O(max(n,m)) & Space Complexity: O(max(n,m))

---
+++ 415.AddStrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder ans=new StringBuilder();
+        int n1=num1.length();
+        int n2=num2.length();
+        
+        int i=n1-1;
+        int j=n2-1;
+        int carry=0;
+       
+        while(i>=0 || j>=0){
+            int a=(i>=0)? num1.charAt(i)-'0' :0;
+            int b=(j>=0)? num2.charAt(j)-'0' :0;
+            int add=a+b+carry;
+            carry=add/10;
+            add=add%10;
+            ans.append(add);
+            i--;
+            j--;
+        }
+
+        while(carry>0){
+            ans.append(carry);
+            carry=carry/10;
+        }
+        return ans.reverse().toString();
+
+
+       
+
+
+    }
+}
