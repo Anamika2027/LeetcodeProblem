a4e0e795bf193632dab654351a5b14e22141234d
{anamika_singh} <ianamikas2017@gmail.com>
2026-02-10 19:47:17 +0530

    Time Complexity: O(n) & Space Complexity: O(1)

---
+++ 2095.DeletetheMiddleNodeofaLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null||head.next==null) return null;
+        ListNode dummy=new ListNode(0);
+        ListNode prev=dummy;
+        dummy.next=head;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            prev=prev.next;
+        }
+        prev.next=slow.next;
+        return head;
+
+    }
+}
