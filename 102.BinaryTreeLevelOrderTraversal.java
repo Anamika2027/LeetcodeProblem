32e0c1e3321bb70f407d2538128b7336ae7aa144
{anamika_singh} <ianamikas2017@gmail.com>
2026-02-21 12:47:05 +0530

    Time Complexity: O(n) & Space Complexity: O(n)

---
+++ 102.BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null) return ans;
+
+        Queue<TreeNode> Q=new LinkedList<>();
+        Q.offer(root);
+        while(!Q.isEmpty()){
+            int size=Q.size();
+            ArrayList<Integer> temp=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode curr=Q.poll();
+                temp.add(curr.val);
+                
+                if(curr.left!=null) Q.offer(curr.left);
+                if(curr.right!=null) Q.offer(curr.right);
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
