864c6fddd311a06a686df4a4a03a9c7d334045bb
{anamika_singh} <ianamikas2017@gmail.com>
2026-03-07 12:41:51 +0530

    Time Complexity: O(n) & Space Complexity: O(1)

---
+++ 152.MaximumProductSubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int max=nums[0];
+        int min=nums[0];
+        int result=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int temp=Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
+            min=Math.min(nums[i],Math.min(max*nums[i],min*nums[i]));
+            max=temp;
+
+            result=Math.max(max,result);
+        }
+
+        return result;  
+    }
+}
