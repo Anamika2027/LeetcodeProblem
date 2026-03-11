0ab687e565636d1386c4ad05e785dcf2381ab29e
{anamika_singh} <ianamikas2017@gmail.com>
2026-03-11 15:32:02 +0530

    Time Complexity: O(n) & Space Complexity: O(n)

---
+++ 503.NextGreaterElementII.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+       
+        int n=nums.length;
+        int[] result=new int[n];
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=2*n-1;i>=0;i--){
+            int index=i%n;
+            while(!st.isEmpty()&&st.peek()<=nums[index]){
+                st.pop();
+            }
+            
+            if(i<n){
+                result[index]=(!st.isEmpty())?st.peek():-1;
+            }
+            st.push(nums[index]);
+        }
+
+        return result;
+    }
+}
