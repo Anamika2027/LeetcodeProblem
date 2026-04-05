d5ab870f75fb4bedb0fcd967620c3b60acfcdabc
{anamika_singh} <ianamikas2017@gmail.com>
2026-04-05 17:32:40 +0530

    Time Complexity: O(N*M) & Space Complexity: O(N*M)

---
+++ 130.SurroundedRegions.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void solve(char[][] board) {
+        int rows=board.length;
+        int cols=board[0].length;
+
+        for(int i=0;i<rows;i++){
+            dfs(board,i,0);
+            dfs(board,i,cols-1);
+        }
+
+        for(int j=0;j<cols;j++){
+            dfs(board,0,j);
+            dfs(board,rows-1,j);
+        }
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }else if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board,int i,int j){
+
+        int rows=board.length;
+        int cols=board[0].length;
+        if(i>=rows||i<0 || j>=cols||j<0||board[i][j]!='O') return;
+
+        board[i][j]='#';
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
+    }
+}
